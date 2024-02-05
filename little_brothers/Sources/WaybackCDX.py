69241ce16dee16e407e86523042d7422d64599ce
Kowalski Van <KowalskiVan@yandex.com>
2024-02-05 22:41:21 +1100

    add Wayback example

--- little_brothers/Sources/WaybackCDX.py
+++ little_brothers/Sources/WaybackCDX.py
@@ -4,5 +4,15 @@ requests.packages.urllib3.disable_warnings()
 
 # https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server
 class Wayback:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.url = 'https://web.archive.org/cdx/search/cdx'
+
+    # ["urlkey","timestamp","original","mimetype","statuscode","digest","length"]
+    def example(self):
+        # Basic Usage
+        url = self.url + '?url=archive.org&limit=100'
+        response = requests.get(url, verify=False)
+        print(response.text)
+
+# wayback = Wayback()
+# wayback.example()
