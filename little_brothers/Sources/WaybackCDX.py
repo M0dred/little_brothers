bef4ee8f133b259afdaebf8391ff0958dd03b2f8
Max Wen <max.wen@unimelb.edu.au>
2024-02-05 14:05:49 +1100

    add Wayback API

---
+++ little_brothers/Sources/WaybackCDX.py
@@ -0,0 +1,8 @@
+import requests
+
+requests.packages.urllib3.disable_warnings()
+
+# https://github.com/internetarchive/wayback/tree/master/wayback-cdx-server
+class Wayback:
+    def __init__(self) -> None:
+        pass
